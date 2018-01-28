903dcd1c41c267b89f0fbb4903cd637c753276b5
Simon Crosland <simon.crosland@gmail.com>
2018-01-28 16:46:07 +0000

    Update shell functions including tmux helper
    functions: Split out bashisms into a separate file, whilst retaining a default set_title(), and add a man() to cope with the deficient behaviour of man no macos when stdout isn't a terminal.
    
    functions.bash: this_file() and this_script() have been broken possibly forever: I want the file that contains the caller, not these functions!  Split the window resizing out of tma() into a helper in preparation for making tma a full function tmux front end.

---
+++ functions.bash
@@ -0,0 +1,61 @@
+#
+# vim: set filetype=sh:
+#
+# Bash functions
+#
+
+# echo the abs path of this file (expanding one level of symlink)
+this_file()
+{
+    local source="${BASH_SOURCE[1]}"  # 1 == caller
+    local file=$(test -L "$source" && readlink "$source" || echo "$source")
+    abs_file "$file" ;
+}
+
+# echo the abs path of the running script $0 (no symlink expansion)
+this_script()
+{
+    local source="${BASH_SOURCE[1]}"  # 1 == caller
+    abs_file "$source" ;
+}
+
+# set terminal title
+set_title()
+{
+    local ansititle="\033]0;${1}\007"
+    if [ -z "$ORIG_PROMPT_COMMAND" ] ; then
+        ORIG_PROMPT_COMMAND="${PROMPT_COMMAND}"
+    fi
+    if [ -z "$1" ] ; then
+        PROMPT_COMMAND="${ORIG_PROMPT_COMMAND}"
+    else
+        PROMPT_COMMAND="echo -ne '${ansititle}'"
+    fi
+}
+
+# tmux attach and resize parent terminal to match the target session
+tmux_resize_to_match_session()
+{
+    local session="$1"
+    details=$(tmux ls -F '#{session_name}:#{session_width}:#{session_height}' | grep -s "^$1:")
+    if [ -z "${details}" ] ; then 
+        echo "error: unknown session '${session}'" >&2
+        return 1
+    fi
+    local _session width height
+    IFS=: read _session width height <<< "${details}"
+    # add one line to allow for the tmux status bar
+    height=$(expr ${height} + 1)
+    local command="\033[8;${height};${width}t"
+    echo -ne "${command}"
+    return 0
+}
+
+tma()
+{
+    local session="$1"
+    if ! tmux_resize_to_match_session "${session}" ; then
+        return
+    fi
+    tmux attach -t "${session}"
+}

0d0c9cd1f091c35ae81f9a2561efdb8ab205c85a
Simon Crosland <simon.crosland@gmail.com>
2018-08-01 15:55:38 +0100

    Add an install script for WSL to help bootstrap
    wsl/README.md:
    wsl/install.sh: A simple install script to help with bootstrapping an
    install and/or updating it.  Probably some common parts should be
    separated and shared with the mac equivalent.

---
+++ wsl/README.md
@@ -0,0 +1,6 @@
+# Support for Debian in Windows Subsystem for Linux (WSL)
+
+This directory contains support files for [Debian][] installed in [WSL][].
+
+[Debian]: https://blogs.msdn.microsoft.com/commandline/2018/03/06/debian-gnulinux-for-wsl-now-available-in-the-windows-store/
+[WSL]: https://docs.microsoft.com/en-us/windows/wsl/install-win10
---
+++ wsl/install.sh
@@ -0,0 +1,62 @@
+#!/bin/bash -e
+
+cd $HOME
+
+if [[ ! -r /etc/debian_version ]] ; then
+    echo "Error: cannot find /etc/debian_version: is this a Debian install?" >&1
+    exit 1
+fi
+
+sudo apt-get update 
+sudo apt-get upgrade
+
+sudo apt-get install aptitude
+
+sudo aptitude install curl
+sudo aptitude install git
+sudo aptitude install ipcalc
+sudo aptitude install jq 
+sudo aptitude install lsb-release
+sudo aptitude install man-db
+sudo aptitude install python python3
+sudo aptitude install ssh
+sudo aptitude install ttf-dejavu
+sudo aptitude install vim-gnome
+
+lsb_release -a
+
+if [[ ! -d $HOME/.ssh ]] ; then
+    echo "Setting up ssh"
+    chmod g+S $HOME/.ssh
+    chmod 2700 $HOME/.ssh
+    ls -ld $HOME/.ssh
+    ssh-keygen -t rsa
+    ssh-keygen -t ecdsa -b 521
+    ls -l $HOME/.ssh
+fi
+
+if [[ ! -r $HOME/.git/config ]] ; then
+    echo "Setting up git"
+    read -p"Enter email for git: " INPUT
+    git config --global user.email "${INPUT}"
+    read -p"Enter username for git: " INPUT
+    git config --global user.name "${INPUT}"
+    git config --global color.pager false
+    git config --global core.excludesfile $HOME/dotfiles/gitignore.global
+    git config --global credential.helper store
+    git config --global web.browser sensible-browser
+    git config --global --list
+fi
+
+if [[ ! -d $HOME/dotfiles ]] ; then
+    echo "Setting up dotfiles"
+    git clone https://github.com/scrosland/dotfiles.git $HOME/dotfiles
+
+    echo ". \$HOME/dotfiles/startup" >$HOME/.bashrc
+    echo ". \$HOME/dotfiles/startup" >$HOME/.profile
+    echo "\$include $HOME/dotfiles/inputrc" >$HOME/.inputrc
+    echo "source \$HOME/dotfiles/vim/vimrc" > $HOME/.vimrc
+    echo "source \$HOME/dotfiles/vim/gvimrc" > $HOME/.gvimrc
+
+    vim -c 'call plugins#bootstrap()'
+fi

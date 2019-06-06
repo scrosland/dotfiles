3cfaa66934658443d84abbac655dcedd484c7786
Simon Crosland <simon.crosland@gmail.com>
2019-06-06 16:02:28 +0100

    Install bash from Homebrew
    Apple announced at WWDC that macOS Catalina (10.15) will switch to using zsh by default. It's apparently because recent bash versions are licensed as GPLv3.
    
    mac/Brewfile: Add bash into the mix.
    
    mac/install.sh: In order to use /usr/local/bin/bash as the login shell it needs to be added into the list of "standard shells" (see man chsh). After that chsh can switch to it.

--- mac/install.sh
+++ mac/install.sh
@@ -54,6 +54,22 @@ run brew cleanup
 
 hash -r
 
+# Install bash
+LOCALBASH=/usr/local/bin/bash
+if [[ -x ${LOCALBASH} ]] ; then
+    if ! grep -q -s ${LOCALBASH} /etc/shells ; then
+        echo "# Adding ${LOCALBASH} to the available shells"
+        echo "# This will require sudo"
+        echo ${LOCALBASH} | sudo tee -a /etc/shells
+    fi
+    CURRENTSHELL=$(dscl . -read ${HOME} UserShell | awk '{print $NF}' -)
+    if [[ ${CURRENTSHELL} != ${LOCALBASH} ]] ; then
+        echo "# Changing shell to ${LOCALBASH} from ${CURRENTSHELL}"
+        run chsh -s ${LOCALBASH}
+        echo "# Close and reopen the terminal to use the new shell"
+    fi
+fi
+
 # Copy apps into the applications folder.
 # This is not using "brew linkapps" because that creates symlinks into /usr
 # which Spotlight will refuse to index.

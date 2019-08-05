ace0a90a3bbaa5f21394ac38283b6fbf1ca8fef7
Simon Crosland <simon.crosland@gmail.com>
2019-08-05 12:49:01 +0100

    mac/install/java.sh: Tidy output and get the latest download URLs.

--- mac/install/java.sh
+++ mac/install/java.sh
@@ -2,7 +2,18 @@
 
 source "$(dirname $0)/functions.shlib"
 
-echo ""
+downloadURL()
+{
+    local infoURL="$1"
+    curl -sSL "${infoURL}" | jq '.binaries|.[]|.installer_link' | tail -1
+}
+
+infoURL()
+{
+    local version="$1"
+    echo "https://api.adoptopenjdk.net/v2/info/releases/${version}?openjdk_impl=hotspot&os=mac&release=latest&type=jdk"
+}
+
 if ls -1 /Library/Java/JavaVirtualMachines | grep -qs ^jdk ; then
     echo "# *** Removing old Oracle JDKs ***"
     sudo rm -rf /Library/Java/JavaVirtualMachines/jdk*
@@ -11,9 +22,17 @@ echo "# Checking for JDK."
 if ! /usr/libexec/java_home --failfast ; then
     echo "Cannot find Java."
 fi
+
+echo ""
 echo "# Default java version."
 java -version
+echo ""
 echo "# All installed JDKs."
 #ls -1 /Library/Java/JavaVirtualMachines
-/usr/libexec/java_home --verbose
+/usr/libexec/java_home --verbose 2>&1 | sed -e '/^$/,$d'
+
+echo ""
 echo "# New JDKs can be downloaded from https://adoptopenjdk.net/"
+echo "# Latest releases of interest:"
+downloadURL $(infoURL openjdk8)
+downloadURL $(infoURL openjdk11)

a23e69b0e27ff9d969ed9ecfc7725d53e2cf7f3f
Simon Crosland <simon.crosland@gmail.com>
2021-05-28 16:29:06 +0100

    Add more go tools
    mac/install/golang.sh:

--- mac/install/golang.sh
+++ mac/install/golang.sh
@@ -3,4 +3,16 @@
 source "$(dirname $0)/functions.shlib"
 
 GO111MODULE=on ; export GO111MODULE
-run go install golang.org/x/tools/gopls@latest
+
+TOOLS=()
+TOOLS+=( golang.org/x/tools/gopls@latest )
+TOOLS+=( github.com/fatih/motion@latest )
+TOOLS+=( github.com/jstemmer/gotags@master )
+TOOLS+=( honnef.co/go/tools/cmd/keyify@master )
+TOOLS+=( honnef.co/go/tools/cmd/staticcheck@latest )
+
+declare -p TOOLS
+
+for tool in "${TOOLS[@]}" ; do
+    run go install "${tool}"
+done

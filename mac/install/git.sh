928a33f207cf703e27922cef2cf8ea7a9b7d4cf2
Simon Crosland <simon.crosland@gmail.com>
2021-07-10 16:03:28 +0100

    git.sh: Configure git pull to remove merge/rebase warning
    mac/install/git.sh: Configure git pull to try and do a fast-forward and
    merge otherwise; and never to do a rebase unless --rebase is given on
    the command line.

--- mac/install/git.sh
+++ mac/install/git.sh
@@ -9,3 +9,13 @@ fi
 if ! git config --global --get commit.verbose >/dev/null 2>&1 ; then
     run git config --global commit.verbose true
 fi
+
+# configure pull to fast-forward when possible and merge otherwise ...
+if ! git config --global --get pull.ff >/dev/null 2>&1 ; then
+    run git config --global pull.ff true
+fi
+
+# ... and not to rebase (unless --rebase given on the command line)
+if ! git config --global --get pull.rebase >/dev/null 2>&1 ; then
+    run git config --global pull.rebase false
+fi

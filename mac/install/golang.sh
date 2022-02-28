5b4f06038990ce2f242ae6d6a4de047e54723d2c
Simon Crosland <simon.crosland@gmail.com>
2022-02-28 20:27:47 +0000

    vscode: add extra go tools
    mac/install/golang.sh: Add in gopkgs and go-outline which the vscode
    plugin for go uses. There is work underway to replace some of these
    with gopls, see https://github.com/golang/vscode-go/issues/1652.

--- mac/install/golang.sh
+++ mac/install/golang.sh
@@ -8,6 +8,10 @@ TOOLS=()
 TOOLS+=( golang.org/x/tools/gopls@latest )
 TOOLS+=( github.com/fatih/motion@latest )
 TOOLS+=( github.com/jstemmer/gotags@master )
+# for vscode until replaced by gopls
+TOOLS+=( github.com/ramya-rao-a/go-outline@latest )
+# for vscode until replace by gopls
+TOOLS+=( github.com/uudashr/gopkgs/v2/cmd/gopkgs@latest )
 TOOLS+=( honnef.co/go/tools/cmd/keyify@master )
 TOOLS+=( honnef.co/go/tools/cmd/staticcheck@latest )
 

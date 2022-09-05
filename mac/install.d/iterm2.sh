7fd767da4f8a3860754e96de611bdba8b0b9eaea
Simon Crosland <simon.crosland@gmail.com>
2022-09-05 15:56:32 +0100

    iterm2: Create the directory before using it!

--- mac/install.d/iterm2.sh
+++ mac/install.d/iterm2.sh
@@ -4,11 +4,14 @@ source "$(dirname $0)/functions.shlib"
 
 # The best GitHub theme I can find:
 #   https://github.com/cdalvaro/github-vscode-theme-iterm
-THEME="${HOME}/Library/Application Support/iTerm2/scrosland/GitHub Light Default.itermcolors"
+SAVE_DIRECTORY="${HOME}/Library/Application Support/iTerm2/scrosland"
+THEME_NAME="GitHub Light Default.itermcolors"
+THEME="${SAVE_DIRECTORY/${THEME_NAME}"
 if [[ ! -r "${THEME}" ]] ; then
+    mkdir -p "${SAVE_DIRECTORY}"
     curl -sSL -o "${THEME}" 'https://raw.githubusercontent.com/cdalvaro/github-theme-iterm/HEAD/GitHub%20Light%20Default.itermcolors'
     echo "# Theme saved as: ${THEME}"
-    echo "# About to install $(basename "${THEME}")"
+    echo "# About to install ${THEME_NAME}"
     sleep 1
     open "${THEME}"
 fi

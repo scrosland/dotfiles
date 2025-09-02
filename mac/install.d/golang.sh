e8e2f0f2e0e52c26344ef2c07f43e617a4aaf57b
Simon Crosland <simon.crosland@gmail.com>
2025-09-02 09:46:10 +0100

    mac/install: Update go packages and remove python
    mac/install:
    mac/install.d/python.sh: Python packages are now installed via brew.
    Remove the unused file.
    
    mac/install.d/golang.sh: Revamp the packages to match current vscode-go
    and vim-go needs.

--- mac/install.d/golang.sh
+++ mac/install.d/golang.sh
@@ -2,22 +2,27 @@
 
 source "$(dirname $0)/functions.shlib"
 
-GO111MODULE=on ; export GO111MODULE
+GO111MODULE=on
+export GO111MODULE
 
 TOOLS=()
-TOOLS+=( golang.org/x/tools/gopls@latest )
-TOOLS+=( github.com/fatih/motion@latest )
-TOOLS+=( github.com/go-delve/delve/cmd/dlv@latest )
-TOOLS+=( github.com/jstemmer/gotags@master )
-# for vscode until replaced by gopls
-TOOLS+=( github.com/ramya-rao-a/go-outline@latest )
-# for vscode until replace by gopls
-TOOLS+=( github.com/uudashr/gopkgs/v2/cmd/gopkgs@latest )
-TOOLS+=( honnef.co/go/tools/cmd/keyify@master )
-TOOLS+=( honnef.co/go/tools/cmd/staticcheck@latest )
+
+# language server
+TOOLS+=(golang.org/x/tools/gopls@latest)
+
+# for vim-go
+TOOLS+=(github.com/fatih/motion@latest)
+TOOLS+=(github.com/go-delve/delve/cmd/dlv@latest)
+TOOLS+=(github.com/jstemmer/gotags@master)
+
+# for vscode-go and vim-go
+TOOLS+=(github.com/golangci/golangci-lint/v2/cmd/golangci-lint@latest)
+TOOLS+=(github.com/mgechev/revive@latest)
+TOOLS+=(golang.org/x/tools/cmd/goimports@latest)
+TOOLS+=(honnef.co/go/tools/cmd/staticcheck@latest)
 
 declare -p TOOLS
 
-for tool in "${TOOLS[@]}" ; do
+for tool in "${TOOLS[@]}"; do
     run go install "${tool}"
 done
--- mac/install.d/python.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-source "$(dirname $0)/functions.shlib"
-
-# Now installed from Brew
-#run pip3 install black

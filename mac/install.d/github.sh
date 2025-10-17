39944aa36953ffc9e2cdfd269a414bfb04745823
Simon Crosland <simon.crosland@gmail.com>
2025-10-17 13:32:32 +0100

    mac/install.d/github.sh: Remove gh-copilot
    The gh-copilot extension is being deprecated and replaced with a
    standalone `copilot` cli. Remove the old one.

--- mac/install.d/github.sh
+++ mac/install.d/github.sh
@@ -10,12 +10,9 @@ if gh auth status --active >${TMPFILE} 2>&1; then
     account=$(awk '/github.com account/ {print $(NF-1)}' ${TMPFILE})
     if [[ ${account} = scroslandhv ]]; then
         echo "# GitHub CLI is authenticated as ${account}."
-        if ! gh extension list | grep -q gh-copilot; then
-            echo "# Installing GitHub Copilot extension."
-            run gh extension install github/gh-copilot
-        else
-            echo "# Upgrading GitHub Copilot extension."
-            run gh extension upgrade gh-copilot
+        if gh extension list | grep -q gh-copilot; then
+            echo "# Removing deprecated GitHub Copilot extension."
+            run gh extension remove github/gh-copilot
         fi
     fi
 else
